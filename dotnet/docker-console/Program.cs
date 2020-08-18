9a1683cf339ef302399b18696ae4a76c2c008a80
Érico de Souza Loewe <erico.loewe@cwi.com.br>
2020-08-18 14:44:59 -0300

    add docker pocs

---
+++ dotnet/docker-console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace docker
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var counter = 0;
+            var max = args.Length != 0 ? Convert.ToInt32(args[0]) : -1;
+
+            while (max == -1 || counter < max)
+            {
+                Console.WriteLine($"Counter: {++counter}");
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
