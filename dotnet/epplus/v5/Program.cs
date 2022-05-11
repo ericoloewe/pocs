bf6b600d6b0192f1f950e92ce2db53e93858fc79
Erico de Souza Loewe <ericoloewe_dbc@getnet.com.br>
2022-05-11 16:47:56 -0300

    add case

--- dotnet/epplus/v5/Program.cs
+++ dotnet/epplus/v5/Program.cs
@@ -10,12 +10,25 @@ namespace v5
         {
             Console.WriteLine("Hello World v5!");
 
-            using ExcelPackage excel = new ExcelPackage(File.OpenRead("../../../../github_example.xlsx"));
-
             ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
 
-            Console.WriteLine($"C29: {excel.Workbook.Worksheets["Summary"].Cells["C29"].Value}");
-            Console.WriteLine($"S6123: {excel.Workbook.Worksheets["Details"].Cells["S6123"].Value}");
+            using ExcelPackage github = new ExcelPackage(File.OpenRead("../../../../github_example.xlsx"));
+
+            Console.WriteLine($"C29: {github.Workbook.Worksheets["Summary"].Cells["C29"].Value}");
+            Console.WriteLine($"S6123: {github.Workbook.Worksheets["Details"].Cells["S6123"].Value}");
+
+            using ExcelPackage getnet = new ExcelPackage(File.OpenRead("../../../../getnet_errors.xlsx"));
+
+            Console.WriteLine($"B1: {getnet.Workbook.Worksheets["Plan1"].Cells["B1"].Value}");
+            Console.WriteLine($"B2: {getnet.Workbook.Worksheets["Plan1"].Cells["B2"].Value}");
+
+            Console.WriteLine($"E1: {getnet.Workbook.Worksheets["Plan1"].Cells["E1"].Value}");
+            Console.WriteLine($"E2: {getnet.Workbook.Worksheets["Plan1"].Cells["E2"].Value}");
+
+            Console.WriteLine($"H1: {getnet.Workbook.Worksheets["Plan1"].Cells["H1"].Value}");
+            Console.WriteLine($"H2: {getnet.Workbook.Worksheets["Plan1"].Cells["H2"].Value}");
+
+            Console.ReadKey();
         }
     }
 }
