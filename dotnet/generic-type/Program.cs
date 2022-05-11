4ded0c9c3133ae077830e13210fe5733dd4fc45b
Erico de Souza Loewe <ericoloewe_dbc@getnet.com.br>
2022-05-11 16:18:56 -0300

    generic

---
+++ dotnet/generic-type/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace generic_type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var basee = new Base<Generic>();
+
+            basee.ASD();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Base<T>
+    {
+        public void ASD()
+        {
+            var generic = typeof(T);
+            var type = Type.GetType($"{generic.FullName}_Hist");
+            var genericHist = Activator.CreateInstance(type);
+
+            Console.WriteLine(generic);
+            Console.WriteLine(type);
+        }
+    }
+
+    class Generic
+    {
+    }
+
+    class Generic_Hist
+    {
+    }
+}
