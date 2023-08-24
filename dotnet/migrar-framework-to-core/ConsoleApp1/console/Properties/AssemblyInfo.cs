f9e5d06a2ba8a61bcc0a66647c1b3a7c9971a6b5
Érico de Souza Loewe <erico.loewe@dbccompany.com.br>
2023-08-24 16:17:04 -0300

    migrar

--- dotnet/migrar-framework-to-core/ConsoleApp1/console/Properties/AssemblyInfo.cs
+++ dotnet/migrar-framework-to-core/ConsoleApp1/console/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // As informações gerais sobre um assembly são controladas por
 // conjunto de atributos. Altere estes valores de atributo para modificar as informações
 // associadas a um assembly.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("console")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("console")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
-[assembly: Guid("b9217c94-803f-4c48-9d30-1058ee6e4c5b")]
+[assembly: Guid("b161903d-8273-4c05-8aa1-6e82db661448")]
 
 // As informações da versão de um assembly consistem nos quatro valores a seguir:
 //
--- dotnet/migrar-framework-to-core/ConsoleApp1/docker/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="docker.MvcApplication" Language="C#" %>
